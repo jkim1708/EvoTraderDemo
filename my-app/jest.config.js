0230409845ec30c017e43b51addcfcbf8ef437fb
jkim1708 <jkim1708@gmail.com>
2024-11-04 12:13:33 +0100

    make test running and being able to build also

--- my-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-        "^.+\\.(ts|tsx)$": "babel-jest"
-    },
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/$1'
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
