39af481104dc2593bf8a1f4499b43c96eb541a4e
jkim1708 <jkim1708@gmail.com>
2025-02-03 10:25:37 +0100

    fix build

--- my-app/components/hooks/useZoomAndPan.ts
+++ my-app/components/hooks/useZoomAndPan.ts
@@ -106,6 +106,7 @@ export const useZoomAndPan = ({
   const resizeObserverCallback = useCallback(
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-expect-error
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     (e) => {
       if (wrapperRef.current) {
         const xAxis = wrapperRef.current.querySelector(
